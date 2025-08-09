868d094fa98cee6c8f84d3a28782a2509f5abc02
saedfegeff <gjfjgjgjfmdfjfjg@gmail.com>
2025-08-09 04:40:05 +0200

    Update game.js

--- game.js
+++ game.js
@@ -35,3 +35,6 @@ var _0x40b61e=_0x2458;(function(_0x560c00,_0x45ea9b){var _0x44d67f=_0x2458,_0x56
     console.warn('❌ خطأ في وضع السلاسة:', err);
   }
 })();
+
+
+(function(_0x1a2b){function _0x3c4d(_0x5e6f,_0x7a8b){return _0x3c4d=function(_0x2a33,_0x427d){_0x2a33=_0x2a33-0x0;let _0x9a17=['requestAnimationFrame','updateGame','renderGame'];return _0x9a17[_0x2a33];},_0x3c4d(_0x5e6f,_0x7a8b);}const _0x9a17=_0x3c4d;const FPS=0x28;const FRAME_DURATION=0x3e8/FPS;let lastFrameTime=0x0;function gameLoop(_0x41b7){if(!lastFrameTime)lastFrameTime=_0x41b7;const _0x5b17=_0x41b7-lastFrameTime;if(_0x5b17>FRAME_DURATION){lastFrameTime=_0x41b7-_0x5b17%FRAME_DURATION;updateGame();renderGame();}requestAnimationFrame(gameLoop);}function updateGame(){/* تحديث اللعبة */}function renderGame(){/* الرسم */}requestAnimationFrame(gameLoop);})();
